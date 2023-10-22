5b36069613ea76ea7e1ab0a7eff08d9db95e3bfe
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-10-22 12:20:06 +0400

    feat: update Product Service API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://857t8nsz3h.execute-api.eu-central-1.amazonaws.com/",
+  bff: "https://3urlc2lae6.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
