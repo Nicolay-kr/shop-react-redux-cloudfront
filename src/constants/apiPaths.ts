ae615a2c81030198dbd3dda5e70491667914c1b9
Mikalai Kryshchanovich <Mikalai_Kryshchanovich@epam.com>
2023-10-26 20:18:37 +0400

    feat: add object through presigned url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://dlo9rojpi6.execute-api.eu-central-1.amazonaws.com/dev",
   bff: "https://3urlc2lae6.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
